f7e74a5eb753fe898b66173954279f23866aca23
Filia Medici <69800575+Filia-Medici-x86-64@users.noreply.github.com>
2020-10-09 13:24:35 -0400

    Create today.py

---
+++ today.py
@@ -0,0 +1,8 @@
+FirstName = input("What is your first name?")
+LastName = input("What is your last name?")
+age = input("What is your age?")
+
+print("Your first name is " + FirstName + "!" )
+print("Your last name is " + LastName + "!")
+print("Your full name is " + FirstName + " " + LastName + "!")
+print("Your age is " + age + "!")
