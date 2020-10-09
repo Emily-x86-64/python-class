d22aafdebcdea987f61fe1787456fdcca21a8265
Filia Medici <69800575+Filia-Medici-x86-64@users.noreply.github.com>
2020-10-09 13:24:12 -0400

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,6 @@
+name = "George"
+age = "65"
+print("There once was a man named " + name)
+print("He was " + age + " years old")
+print("He liked the name " + name)
+print("But he hated being " + age + " years old")
